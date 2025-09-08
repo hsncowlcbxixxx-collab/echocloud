5d196007ad2345efab3709af6728f50cba56e422
hsncowlcbxixxx-collab <hsncowlcbxixxx@gmail.com>
2025-09-08 14:53:52 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Plans from "./Plans";
+import "./index.css"; // your Tailwind or custom styles
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Plans />
+  </React.StrictMode>
+);
+
